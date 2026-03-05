99a7bf8c3e9ab9db32df4976d0e6cc9d0d367581
adaml <adaml@10.3.250.74>
2026-03-05 09:24:00 +0700

    pertama

--- src/main/java/PERTEMUAN2_methodparameter/mahasiswa.java
+++ src/main/java/PERTEMUAN2_methodparameter/mahasiswa.java
@@ -1,40 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package PERTEMUAN2_methodparameter;
 
-public class mahasiswa {
-    public String nama;
-    public String nim;
-    public int umur;
+/**
+ *
+ * @author adaml
+ */
+class mahasiswa {
     
-    public void cetakBiodata(){
-        System.out.println("====================");
-        System.out.println("Nama: "+nama);
-        System.out.println("NIM: "+nim);
-        System.out.println("Umur: "+umur);
-        System.out.println("====================");0
-    }
-    
-    public void sapa(String yangDiSapa){
-        System.out.println("====================");
-        System.out.println("Assalamualaikum "+yangDiSapa);
-        System.out.println("Nama saya "+nama);
-        System.out.println("====================");
-    }
-    
-    public double nilaiAngka(double UAS, double UTS, double tugas, double kuis){
-        double nilaiAkhir = 0.3*UAS+0.3*UTS+0.2*tugas+0.2*kuis;
-        return nilaiAkhir;
-    }
-    
-    public String nilaiHuruf(double nilaiAkhir){
-        String nilaiHuruf = "";
-        
-        if (nilaiAkhir >= 80){
-            nilaiHuruf = "A";
-        }
-        else{
-            nilaiHuruf = "B";
-        }
-        
-        return nilaiHuruf;
-    }
 }
